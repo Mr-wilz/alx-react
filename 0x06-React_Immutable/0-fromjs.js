3f8328ffebe5a8f0ecc5a9696cb486ae4a9aa5c2
Mr-wilz <juiciwhilf@gmail.com>
2024-11-22 00:48:20 +0000

    fromjs

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,11 @@
+import { fromJS } from 'immutable';
+
+/**
+ *Converts a plain JavaScript object into an Immutable Map
+ * @param {Object} object - The JavaScript object to be converted
+ * @returns {Map} - Immutable Map representing the object
+ */
+
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
